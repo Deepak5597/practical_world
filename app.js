df790441b4f488843d3b5b3daf2bf5457f013847
Deepak Bisht <32607176+Deepak5597@users.noreply.github.com>
2020-07-22 11:43:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const engine = require('ejs-mate');
 
+const postConfig = require('./config/post.config');
+
+app.engine('ejs',engine);
 app.set('view engine','ejs');
 app.use(cors());
 app.use('/css',express.static('css'));
+app.use('/posts/css',express.static('css'));
 app.use('/images',express.static('images'));
+app.use('/posts/images',express.static('images'));
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index',{'postConfig':postConfig.posts});
+});
+app.get('/posts/:postHeading/:postSubheading',(req,res)=>{
+    res.render('posts',{postPath:'posts/'+req.params.postHeading+'/'+req.params.postSubheading,'postConfig':postConfig.posts,'mainHeading':req.params.postHeading,'subHeading':req.params.postSubheading},);
 });
 
 const appConfig = require('./config/app.config');
---
+++ config/post.config.js
@@ -0,0 +1,52 @@
+module.exports={
+    posts:[
+        {
+            mainHeadingKey:'jenkins',
+            mainHeadingLabel:'Jenkins',
+            topics:[
+                {
+                    topicHeadingKey:'install_jenkins_on_centos',
+                    topicHeadingLabel:'Installing Jenkins on centos'
+                },
+                {
+                    topicHeadingKey:'install_jenkins_on_windows',
+                    topicHeadingLabel:'Installing Jenkins on windows'
+                },
+                {
+                    topicHeadingKey:'install_jenkins_on_ubuntu',
+                    topicHeadingLabel:'Installing Jenkins on ubuntu'
+                },
+                {
+                    topicHeadingKey:'create_first_job_in_jenkins',
+                    topicHeadingLabel:'Creating your first job with Jenkins'
+                }
+            ]
+        },
+        {
+            mainHeadingKey:'node',
+            mainHeadingLabel:'Node',
+            topics:[
+                {
+                    topicHeadingKey:'install_node_on_centos',
+                    topicHeadingLabel:'Installing Node on centos'
+                },
+                {
+                    topicHeadingKey:'install_node_on_windows',
+                    topicHeadingLabel:'Installing Jenkins on windows'
+                },
+                {
+                    topicHeadingKey:'install_node_on_ubuntu',
+                    topicHeadingLabel:'Installing Node on ubuntu'
+                },
+                {
+                    topicHeadingKey:'welcome_project_using_node',
+                    topicHeadingLabel:'Creating Welcome project using node'
+                },
+                {
+                    topicHeadingKey:'ejs_template_for_node',
+                    topicHeadingLabel:'Using EJS templating engine for node'
+                }
+            ]
+        },
+    ]
+}
